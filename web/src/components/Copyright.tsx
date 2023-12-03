bcf9e6bba2bb1ea4f849c1d38002d8c0890b8939
enriqTS <henrcraft@gmail.com>
2023-12-03 09:33:15 -0300

    first commit

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,24 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito por{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://github.com/enriqTS"
+      >
+        Henrique{' '}
+      </a>
+      e{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://github.com/MTHSKM"
+      >
+        Mateus
+      </a>
+    </div>
+  )
+}
